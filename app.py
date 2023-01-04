1fa3dcc4b18c3cbbcfae3b5257ca90b3a7fbb2bb
Shawneau <51348013+Shawneau@users.noreply.github.com>
2023-01-03 23:14:10 -0500

    Remove images that were causing problems

--- app.py
+++ app.py
@@ -25,8 +25,6 @@ local_css("style/style.css")
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_cmaqoazd.json")
-img_contact_form = Image.open("images/yt_contact_form.png")
-img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
 # ---- HEADER SECTION ----
 with st.container():
